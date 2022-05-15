39b6075bbabeb00fe92cb1ae3fe4b2a5c047c355
GolubeykovAnton <ds6580@yandex.ru>
2022-05-15 22:39:59 +0300

    Initial Commit

---
+++ randomUsers/randomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomUsersApp.swift
+//  randomUsers
+//
+//  Created by Антон Голубейков on 15.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct randomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
